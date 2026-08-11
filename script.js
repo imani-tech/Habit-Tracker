1cd6d50ea12442f3dbb3c199c3715832dfd5934a
imani-tech <imanimuthoni9@gmail.com>
2026-08-11 11:18:03 +0300

    changed somethings

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const totalHabits = document.getElementById("totalHabitsText")
 const habitsCompletedToday = document.getElementById("completedTodayText")
 const totalHabitsCompleted = document.getElementById("totalHabitsCompletedText")
 const error = document.getElementById("error")
+let idNum = 0
+let habitArray = []  
+
 
 function verify(){
   if(habitInput.value === "" || habitInput.value.length < 3){
@@ -19,10 +22,10 @@ function verify(){
   return true
 }
 class Habit{
-    constructor(name, category, isCompleted, date){
+    constructor(name, category, id, date){
+        this.id = id
         this.name = name
-        this.category = category
-        this.isCompleted = isCompleted
+        this.category = category        
         this.date = date
     }    
 }
@@ -37,43 +40,57 @@ function displayHabitsAndCategory(){
     displayHabit.append(nameP)
     displayHabit.append(categoryP)    
     date()
-    checkHabit()       
-    deleteBtn()
-   }) 
-}  
-function deleteBtn(){
+    checkHabit(nameP, categoryP)       
+    deleteBtn(habit)      
+   })   
+} 
+
+saveBtn.addEventListener(("click"), (e) =>{ 
+   e.preventDefault()
+  if (!verify()) return;     
+  const newHabit = new Habit(habitInput.value, categorySelection.value, idNum) 
+  habitArray.push(newHabit)
+  displayHabitsAndCategory();  
+  console.log(habitArray)
+  countHabit()
+  idNum++  
+ })
+
+function deleteBtn(habit){
     let deleteBtn = document.createElement("button")
     deleteBtn.innerHTML= `<p><i class="fa-solid fa-trash"></i></p>`
     deleteBtn.classList.add("trashBtn")
     displayHabit.append(deleteBtn) 
     deleteBtn.addEventListener(("click"), () => {
-        
+        let habitObjId = habit.id
+        const habitResultArray = habitArray.filter((habit) => habitObjId !== habit.id)
+      console.log(habitResultArray)
+      habitArray = habitResultArray     
+      displayHabitsAndCategory()
+      countHabit()      
     })   
 }
-function checkHabit(habitPElement){
-    let checkHabitBtn = document.createElement("button")
-    checkHabitBtn.innerHTML= `<p><i class="fa-regular fa-face-frown"></i></p>`
-    checkHabitBtn.classList.add("checkBtn")
-    displayHabit.append(checkHabitBtn)
-    checkHabitBtn.addEventListener(("click"), () => {
-    habitPElement.classList.toggle('check')     
-    }) 
-} 
-  let habitArray = []    
- saveBtn.addEventListener(("click"), (e) =>{ 
-   e.preventDefault()
-  if (!verify()) return;     
-  const newHabit = new Habit(habitInput.value, categorySelection.value) 
-  habitArray.push(newHabit)
-  displayHabitsAndCategory();  
-  console.log(habitArray)
-  countHabit()
- })
 
+function checkHabit(nameP, categoryP){
+    let checkBox = document.createElement("input")    
+    checkBox.type = "checkbox"
+    displayHabit.append(checkBox)
+    checkBox.addEventListener(("click"), () => {        
+    nameP.classList.toggle("completed-habit")
+    categoryP.classList.toggle("completed-habit")      
+    
+    }) 
+    checkIfHabitCompleted = false;
+} 
+   
+function countCompletedHabit(checkIfHabitCompleted){   
+    let complete = 0    
+    totalHabitsCompleted.textContent = 0   
+}
   function countHabit(){
     let num = habitArray.length
     totalHabits.textContent = num
-    console.log(num)
+    console.log("number of habits"+ " "+ num)
 } 
 function date(){
 const today = new Date();
@@ -83,3 +100,4 @@ const todayName = days[dayNum];
 console.log(todayName);
 displayHabit.append(todayName)
 }
+
